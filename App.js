71786cb3b9fb55294f10771318b6da92d8eca1a8
Pratap Lamichhane <pratap@Prataps-MBP.attlocal.net>
2019-01-10 18:09:38 -0600

    First commit

--- App.js
+++ App.js
@@ -1,37 +1,73 @@
 
 import React, { Component } from 'react';
-import { AppRegister, StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { AppRegister, StyleSheet, Text, View, Image } from 'react-native';
 
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover', // or 'stretch'
-        justifyContent: 'center',
-    },
-    loginForm: {
-        backgroundColor: 'transparent',
-        alignItems: 'center',
-    },
+export default class Login extends Component {
+    render() {
+        return (
+            <View style = {styles.container}>
+               <View style = {styles.backgroundContainer}>
+                  <Image style = {styles.backdrop} source = {require('./images/vape.jpg')} resizeMode = 'cover'/>
+               </View>
+               <View style = { styles.overlay}>
+                    <Image
+                        style={styles.logo}
+                        source={require('./images/smoke.jpg')}/>
+                    <Text style={styles.title}>Ada Smoke Shop</Text>
+               </View>
+            </View>
+        );
 
-    text: {
-       fontSize: 30,
-       fontWeight: 'bold',
-    }
-});
-
-export default class adasmokeshop extends Component {
-    render () {
-    return (
-        <View style = { styles.container}>
-             <ImageBackground source = {{uri: 'https://smokefreecasinos.org/wp-content/uploads/2018/04/smoke-69124_1280.jpg'}} style={styles.backgroundImage}>
-                <View style = { styles.loginForm }>
-                     <Text style =  {styles.text }>Ada Smoke Shop</Text>
-                </View>
-             </ImageBackground>
-        </View>
-    );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+    },
+    backgroundContainer: {
+        flex:1,
+        position: 'absolute',
+        top:0,
+        bottom:0,
+        left:0,
+        right:0
+        //width: '100%',
+        //height: '100%'
+    },
+    overlay: {
+        opacity: 0.5,
+        backgroundColor: '#000000',
+        flexDirection: 'column',
+        top:100
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center',
+    },
+    logo: {
+        backgroundColor: '#000000',
+        top:150,
+        left: 50,
+        width: 200,
+        height: 200
+    },
+    backdrop: {
+    flex:1,
+
+    },
+    title: {
+        color: '#B03A2E',
+        marginTop:150,
+        width:300,
+        height:300,
+        fontSize: 30,
+        fontStyle: 'italic',
+        fontFamily: 'Baskerville',
+        textAlign: 'center',
+        opacity: 10
+    },
+
+});
